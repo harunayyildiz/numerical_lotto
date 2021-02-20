926f0d251b4746e5bcc445a47bdaf2c64e17f1c5
Harun Ayyıldız <harunayyildizz15@gmail.com>
2021-02-20 03:40:22 +0300

    Constant

---
+++ lib/constant.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+//Api Base url;
+const String baseUrl = 'http://localhost:5001/';
+//Color
+const mainColor = Color(0xFF0F0D32);
+
+//TextFamily
+const String textFamilyVogue = 'Vogue';
+const String textFamilyMontserrat = 'Montserrat';
+
+//TextStyle
+const textTextStyle = TextStyle(
+  color: mainColor,
+  fontFamily: textFamilyMontserrat,
+  fontSize: 17,
+);
+
+const textTextStyleVogue20 = TextStyle(
+  color: mainColor,
+  fontSize: 20,
+  letterSpacing: 2,
+);
+
+//IMAGE_PATH
+const SPLASH_LOTTIE = 'numerical_lotto';
+const SPLASH_IMAGE = 'assets/images/numerical_lotto.png';
+const GUESS_SVG = 'assets/images/guess.svg';
